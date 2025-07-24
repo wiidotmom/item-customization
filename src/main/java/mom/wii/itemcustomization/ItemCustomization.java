65f9febd21c658d8e01f188ff2ee6fb525a462cb
igalaxy <hey@igalaxy.dev>
2025-07-24 16:06:24 -0700

    smithing template loot table

--- src/main/java/mom/wii/itemcustomization/ItemCustomization.java
+++ src/main/java/mom/wii/itemcustomization/ItemCustomization.java
@@ -1,7 +1,11 @@
 package mom.wii.itemcustomization;
 
+import eu.pb4.polymer.resourcepack.api.PolymerResourcePackUtils;
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.loot.v3.LootTableEvents;
+import net.minecraft.loot.LootPool;
+import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,13 +16,10 @@ public class ItemCustomization implements ModInitializer {
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	private static final Identifier TRAIL_RUINS_RARE_LOOT_TABLE = Identifier.of("minecraft:archaeology/trail_ruins_rare");
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
+		PolymerResourcePackUtils.addModAssets(MOD_ID);
 	}
 }
