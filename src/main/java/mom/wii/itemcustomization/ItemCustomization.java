d2366ca82874b6ed6a5bc15fc15603408fc5f934
igalaxy <hey@igalaxy.dev>
2025-07-24 15:36:53 -0700

    fix mod id

--- src/main/java/mom/wii/itemcustomization/ItemCustomization.java
+++ src/main/java/mom/wii/itemcustomization/ItemCustomization.java
@@ -6,7 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ItemCustomization implements ModInitializer {
-	public static final String MOD_ID = "item-customization";
+	public static final String MOD_ID = "igalaxy_item_customization";
 
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
